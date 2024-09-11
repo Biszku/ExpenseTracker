882f04697327d125bf72aad1d304d0417261b9a0
Biszku <daniel.bivhuniak@gmail.com>
2024-09-11 22:38:10 +0200

    created beta command handler

---
+++ src/CommandHandler.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class CommandHandler {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    Stack<String> commands = new Stack<>();
+
+    public CommandHandler() {}
+
+    public String readCommand() {
+        List<String> commands = new ArrayList<>(List.of(scanner.nextLine().split(" ")));
+        Collections.reverse(commands);
+
+        for (String command : commands) {
+            this.commands.push(command);
+
+        }
+
+        String mainCommand = this.commands.pop();
+        if (!mainCommand.equals("expense-tracker")) {
+            return "Unknown command";
+        }
+
+        return this.commands.pop();
+    }
+}
