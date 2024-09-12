05ee0001ebf5baf3ee1268f0871827ef981567ca
Biszku <daniel.bivhuniak@gmail.com>
2024-09-12 23:21:32 +0200

    Changed initialization

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        new TaskTracker().start();
+        TaskTracker taskTracker = new TaskTracker();
+        taskTracker.start();
     }
 }
