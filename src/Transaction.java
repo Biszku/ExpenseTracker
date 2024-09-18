1f6b2cea57fd2a06ffe0c3ed3608d43b3fbfc2d4
Biszku <daniel.bivhuniak@gmail.com>
2024-09-18 19:55:30 +0200

    changed data structure

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,23 +1,13 @@
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Transaction {
-
-    private int id;
-    private String date;
-    private String description;
-    private double amount;
+public record Transaction (int id, String date, String description, double amount) {
 
     private static DateTimeFormatter formatter =
             DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
 
-    public Transaction(int id, String description, double amount) {
-        this.id = id;
-        date = LocalDateTime.now().format(formatter);
-        System.out.println(date);
-        this.description = description;
-        this.amount = amount;
+    public Transaction (int id, String description, double amount) {
+        this(id, LocalDateTime.now().format(formatter), description, amount);
     }
 }
